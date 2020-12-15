c75c4eb101ddaae03f19bfa91c811750e189ee9a
Daniel Diaz <danieldiazgonza52@gmail.com>
2020-12-15 17:16:16 -0500

    Modified play-pause

--- audio/play-pause.sh
+++ audio/play-pause.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if grep -q "suspend cause: USER" <(pacmd info); then
-  pacmd "suspend 0"
-  pactl set-sink-mute 1 0
-else
-  pacmd "suspend 1"
-  pactl set-sink-mute 1 1
-fi
-
+playerctl play-pause
